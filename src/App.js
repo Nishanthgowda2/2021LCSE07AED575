d20706648c147df9e0677b147c18a40d1c494f9e
NISHANTH B G <124962366+Nishanthgowda2@users.noreply.github.com>
2024-08-21 20:04:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import ProductsList from './components/ProductList';
+import ProductDetail from './components/ProductDetails';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedProduct, setSelectedProduct] = useState(null);
+
+    const handleProductSelect = (product) => {
+        setSelectedProduct(product);
+    };
+
+    return (
+        <div className="App">
+            <h1>Top Products</h1>
+            <ProductsList onSelectProduct={handleProductSelect} />
+            <ProductDetail product={selectedProduct} />
+        </div>
+    );
 }
 
 export default App;
+
